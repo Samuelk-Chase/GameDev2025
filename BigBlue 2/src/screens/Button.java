ccd4ac9237d56a48a40d6e03a50ccbfaafa7eec8
Zach Redfearn <zachattacked02@gmail.com>
2025-03-27 19:40:01 -0600

    An attempt to fix the repo

--- BigBlue 2/src/screens/Button.java
+++ BigBlue 2/src/screens/Button.java
@@ -5,42 +5,41 @@ import edu.usu.graphics.Font;
 import edu.usu.graphics.Graphics2D;
 import edu.usu.graphics.Rectangle;
 
-public class Button {
-    private final KeyboardHandler.KeyAction action;
+public abstract class Button {
     private final Rectangle borderRectangle;
     private final Rectangle rectangle;
     private final Graphics2D graphics;
     private static final float borderPercentage = 0.05f;
     protected static final float textHeightPercentage = 0.75f;
     protected static final Font font = new Font("resources/fonts/Roboto-Bold.ttf", 64, true);
-    protected static final Color backgroundColor = new Color(0.25f, 0.25f, 0.25f);
-    protected static final Color highlightColor = new Color(0.35f, 0.35f, 0.35f);
-    private final String text;
+    protected static final Color backgroundColor = new Color(0.2f, 0.3f, 0.4f); // Bluish-gray for normal state
+    protected static final Color highlightColor = new Color(0.3f, 0.4f, 0.5f); // Lighter bluish-gray for selected state
+    protected String text;
     private final float textX;
     private final float textY;
     private final float textHeight;
 
+    public interface ButtonCreator {
+        Button create(float x, float y, float width, float height, String text, Graphics2D graphics);
+    }
 
-    public Button(float x, float y, float width, float height, String text, KeyboardHandler.KeyAction action, Graphics2D graphics) {
+    public Button(float x, float y, float width, float height, String text, Graphics2D graphics) {
         borderRectangle = new Rectangle(x, y, width, height);
         float borderWidth = height * borderPercentage;
         rectangle = new Rectangle(x + borderWidth, y + borderWidth, width - 2 * borderWidth, height - 2 * borderWidth);
         textHeight = height * textHeightPercentage;
-        textY = y + (height - textHeight) / 2f;
+        textY = y + (height - textHeight) / 3f;
         float textWidth = font.measureTextWidth(text, textHeight);
-        textX = x + (width - textWidth) / 2f;
+        textX = x + (width - textWidth) / 3f;
         this.text = text;
-        this.action = action;
         this.graphics = graphics;
     }
 
-    public void click(double elapsedTime) {
-        action.run(elapsedTime);
-    }
+    public abstract void click(double elapsedTime);
 
-    public void render(Color buttonColor) {
-        graphics.draw(borderRectangle, Color.BLACK);
+    public void render(Color borderColor, Color buttonColor, Color textColor) {
+        graphics.draw(borderRectangle, borderColor);
         graphics.draw(rectangle, buttonColor);
-        graphics.drawTextByHeight(font, text, textX, textY, textHeight, Color.GREEN);
+        graphics.drawTextByHeight(font, text, textX, textY, textHeight, textColor);
     }
 }
--- BigBlue 2/src/screens/MenuScreen.java
+++ BigBlue 2/src/screens/MenuScreen.java
@@ -2,6 +2,8 @@ package screens;
 
 import edu.usu.graphics.Color;
 import edu.usu.graphics.Graphics2D;
+import edu.usu.graphics.Rectangle;
+import edu.usu.graphics.Texture;
 
 import java.util.ArrayList;
 
@@ -10,16 +12,25 @@ import static org.lwjgl.glfw.GLFW.*;
 public class MenuScreen extends Screen {
     private int buttonIndex = 0;
     private final ArrayList<Button> buttons = new ArrayList<>();
+    private static final Color NORMAL_BORDER_COLOR = Color.BLACK;
+    private static final Color SELECTED_BORDER_COLOR = Color.WHITE;
+    private static final Color NORMAL_TEXT_COLOR = Color.WHITE;
+    private static final Color SELECTED_TEXT_COLOR = Color.YELLOW;
+    private Texture backgroundTexture;
+    private float textureWidth;
+    private float textureHeight;
 
-    public record ButtonBundle(String text, KeyboardHandler.KeyAction action) {
-
+    public record ButtonBundle(String text, Button.ButtonCreator creator) {
     }
 
     public MenuScreen(Graphics2D graphics) {
         super(graphics);
-        forceAction(GLFW_KEY_UP, (_) -> {buttonIndex = (--buttonIndex) >= 0 ? buttonIndex : buttons.size() - 1;});
-        forceAction(GLFW_KEY_DOWN, (_) -> {buttonIndex = (++buttonIndex) % buttons.size();});
-        forceAction(GLFW_KEY_ENTER, (double elapsedTime) -> {buttons.get(buttonIndex).click(elapsedTime);});
+        backgroundTexture = new Texture("resources/images/image.png");
+        textureWidth = backgroundTexture.getWidth();
+        textureHeight = backgroundTexture.getHeight();
+        forceAction(GLFW_KEY_UP, (_) -> buttonIndex = (--buttonIndex) >= 0 ? buttonIndex : buttons.size() - 1);
+        forceAction(GLFW_KEY_DOWN, (_) -> buttonIndex = (++buttonIndex) % buttons.size());
+        forceAction(GLFW_KEY_ENTER, (double elapsedTime) -> buttons.get(buttonIndex).click(elapsedTime));
     }
 
     public void addButtons(float buttonHeight, ButtonBundle[] buttonInfo) {
@@ -34,7 +45,7 @@ public class MenuScreen extends Screen {
         float buttonTop = top + (height - (buttonHeight + buttonMargin) * buttonInfo.length + buttonMargin) / 2f;
         float buttonLeft = left + (width - buttonWidth) / 2f;
         for (ButtonBundle info : buttonInfo) {
-            Button button = new Button(buttonLeft, buttonTop, buttonWidth, buttonHeight, info.text, info.action, graphics);
+            Button button = info.creator.create(buttonLeft, buttonTop, buttonWidth, buttonHeight, info.text, graphics);
             buttons.add(button);
             buttonTop += buttonHeight + buttonMargin;
         }
@@ -47,23 +58,40 @@ public class MenuScreen extends Screen {
 
     @Override
     public void processInput() {
-
     }
 
     @Override
     public void screenUpdate(double elapsedTime) {
-
     }
 
     @Override
     public void render() {
-        graphics.draw(screenRectangle, Color.PURPLE);
-        for (int i = 0; i < buttonIndex; i++) {
-            buttons.get(i).render(Button.backgroundColor);
+        graphics.draw(screenRectangle, Color.BLACK);
+
+        if (backgroundTexture != null) {
+            float textureAspect = textureWidth / textureHeight;
+            float screenAspect = width / height;
+            float scale;
+            if (textureAspect > screenAspect) {
+                scale = height / textureHeight;
+            } else {
+                scale = width / textureWidth;
+            }
+            float backgroundScaleFactor = 1.f;
+            scale *= backgroundScaleFactor;
+            float scaledWidth = textureWidth * scale;
+            float scaledHeight = textureHeight * scale;
+            float destX = left + (width - scaledWidth) / 2;
+            float destY = top + (height - scaledHeight) / 2;
+            Rectangle destRect = new Rectangle(destX, destY, scaledWidth, scaledHeight);
+            graphics.draw(backgroundTexture, destRect, Color.WHITE);
         }
-        buttons.get(buttonIndex).render(Button.highlightColor);
-        for (int i = buttonIndex + 1; i < buttons.size(); i++) {
-            buttons.get(i).render(Button.backgroundColor);
+        for (int i = 0; i < buttons.size(); i++) {
+            if (i == buttonIndex) {
+                buttons.get(i).render(SELECTED_BORDER_COLOR, Button.highlightColor, SELECTED_TEXT_COLOR);
+            } else {
+                buttons.get(i).render(NORMAL_BORDER_COLOR, Button.backgroundColor, NORMAL_TEXT_COLOR);
+            }
         }
     }
 }
